2a412ee4896aa8b60426c3d19f8fb0bc8aec7e13
raphou <raphael.valletscoca@gmail.com>
2018-10-09 16:47:57 +0200

    new commit msg

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask 
+from flask_restplus import Resource, Api, Namespace, fields 
+from urllib.parse import urlparse
+    
+# Initialize Flask
+app = Flask(__name__)
+api = Api(app, 	title='Spoky API', 
+                description='Simple API to control Spoky IOT devices', 
+                default="Spoky", 
+                default_label="Spoky related operations")
+spoky = Namespace('Spoky', path="/spoky")
+api.add_namespace(spoky)
